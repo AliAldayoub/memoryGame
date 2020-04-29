4e9448a9fd128ca881d2f19fd206679237eba0e5
AliAldayoub <ali.dy111234@gmail.com>
2020-04-29 20:12:31 +0300

    cached img

--- js/main.js
+++ js/main.js
@@ -110,6 +110,30 @@ const Fruits = [
 		'grizman-min.jpg',
 		'grizman-min.jpg'
 	];
+function preloadImages(array) {
+	if (!preloadImages.list) {
+		preloadImages.list = [];
+	}
+	var list = preloadImages.list;
+	for (var i = 0; i < array.length; i++) {
+		var img = new Image();
+		img.onload = function() {
+			var index = list.indexOf(this);
+			if (index !== -1) {
+				// remove image from the array once it's loaded
+				// for memory consumption reasons
+				list.splice(index, 1);
+			}
+		};
+		list.push(img);
+		img.src = array[i];
+	}
+}
+
+preloadImages(Fruits);
+preloadImages(CardNumber);
+preloadImages(players);
+preloadImages(webDev);
 LetsGetStarted.addEventListener('click', clearOverlay);
 
 startGameButton.addEventListener('click', startGame);
